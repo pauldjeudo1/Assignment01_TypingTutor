59c3ff79ea53a598149420bf10870b3775e112a9
Paul Djeudo <2586403@edu.vaniercollege.qc.ca>
2026-09-25 21:33:54 -0400

    Added a TextField and prepared the list of sample text for user to type. (event handler is to be modified)

--- src/main/java/com/mycompany/assignment01/App.java
+++ src/main/java/com/mycompany/assignment01/App.java
@@ -22,31 +22,53 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        //creating controls and layout
         BorderPane root = new BorderPane();
         root.setPadding(new Insets(10));
         Keyboard keyboard = new Keyboard();
-        Label textToType = new Label("1 of 6");
-        Label notHandled = new Label("Not Handled");
-        notHandled.setStyle("-fx-text-fill: red");
         TextField tf = new TextField();
-        tf.setScaleX(100);
+        tf.setPrefWidth(400);
+        Label textToType = new Label("");
+        Label notHandled = new Label("");
+        notHandled.setStyle("-fx-text-fill: red");
+        Label correctLabel = new Label("");
+        Label incorrectLabel = new Label("");
         
+        //creating array of sample texts
+        String[] sampleTexts = {
+            "Try typing this text. Do it as quickly and accurately as you can.",
+            "Next type another line of input data.",
+            "The quick brown fox jumps over the lazy dog.",
+            "Five big quacking zephyrs jolt my wax bed.",
+            "Sympathizing would fix Quaker objectives.",
+            "A large fawn jumped quickly over white zinc boxes."
+        };
+
         VBox topBox = new VBox(5, textToType, tf);
-        VBox bottomBox = new VBox(5, notHandled);
         topBox.setAlignment(Pos.CENTER);
         root.setTop(topBox);
+        VBox bottomBox = new VBox(5, notHandled, correctLabel,
+        incorrectLabel);
+        root.setBottom(bottomBox);
+
+        
         root.setCenter(keyboard.getView());
-        
+
         Scene scene = new Scene(root, 640, 480);
-        
+
         scene.setOnKeyPressed(e -> {
-        Button btn = keyboard.getKeyMap().get(e.getCode());
-        if (btn != null) {
-            btn.setStyle("-fx-background-color: blue");
-        } else {
-            bottomBox.setAlignment(Pos.CENTER);
-            root.setBottom(bottomBox);
-        }
+            StringBuilder typedText = new StringBuilder();
+            
+            Button btn = keyboard.getKeyMap().get(e.getCode());
+            if (btn != null) {
+                btn.setStyle("-fx-background-color: blue");
+                // Append the character to the StringBuilder and update the TextField
+                String keyText = e.getCode().getName().toLowerCase();
+                typedText.append(keyText);
+                tf.setText(typedText.toString());
+            } else {
+                notHandled.setText("Not handled");
+            }
         });
 
         scene.setOnKeyReleased(e -> {
@@ -54,15 +76,17 @@ public class App extends Application {
             if (btn != null) {
                 btn.setStyle("");
             }
+            notHandled.setText("");
         });
-        
+
         stage.setTitle("Typing Tutor");
         stage.setScene(scene);
         stage.show();
+        root.requestFocus();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
+    
 }
